3bc86dee89aa332bf66c259d9d750346d000de0e
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2014-02-21 03:10:01 -0300

    fix license aggrement validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,6 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
-        :password_confirmation, :license_agrement)
+        :password_confirmation, :license_aggrement)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ActiveRecord::Base
 
   attr_reader :license_aggrement
 
-  validates :password, :confirmation => true
-  validates :password, :presence => true, :on => :create
-  validates :email, :name,  :presence => true
-  validates :email, :uniqueness => true
-  validates :license_aggrement, :acceptance => true
+  validates :email, :name,      :presence     => true
+  validates :email,             :uniqueness   => true
+  validates :password,          :confirmation => true
+  validates :password,          :presence     => true, :on => :create
+  validates :license_aggrement, :acceptance   => true, :on => :create
 
 end
