a6ea261b08b695e101e37259d90784cfb396c718
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2014-02-21 21:38:03 -0300

    fix broken tests and add more

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -21,10 +21,22 @@ describe ApplicationHelper do
   end
 
   describe "#link_to_modal" do
-    let(:anchor) { '<a href="#some-id" class="button" data-components="modal">Open Modal</a>' }
-
-    it "renders anchor tag with data-modal='open'" do
-      expect(helper.link_to_modal 'Open Modal', '#some-id', :class => 'button' ).to eq anchor
+    context "regular modal" do
+      let(:anchor) { '<a href="#some-id" class="button" data-components="modal" data-modal=\'{}\'>Open Modal</a>' }
+      it "renders modal component" do
+        expect(helper.link_to_modal 'Open Modal', '#some-id', :html => { :class => 'button'} ).to eq anchor
+      end
     end
+
+    context "remote modal" do
+      let(:link) { helper.link_to_modal 'Open Modal', '#some-id', :remote => true }
+      it { expect(link).to include 'data-modal=\'{"remote":true}\'' }
+    end
+
+    context "autoload and prevent_close" do
+      let(:link) { helper.link_to_modal 'Open Modal', '#some-id', :autoload => true, :prevent_close => true }
+      it { expect(link).to include 'data-modal=\'{"autoload":true,"prevent_close":true}\'' }
+    end
+
   end
 end
