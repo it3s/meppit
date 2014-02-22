4b363c0204175f60c134d5e84df112e547eef8e4
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2014-02-22 14:25:40 -0300

    add spec to pages controller

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,4 +2,12 @@ require 'spec_helper'
 
 describe PagesController do
 
+  describe "GET frontpage" do
+    before { get :frontpage }
+
+    it { expect(response).to render_template :frontpage }
+
+    it { expect(assigns :news).to_not be_nil }
+  end
+
 end
