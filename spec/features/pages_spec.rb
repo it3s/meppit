19f6355f7b4fbf11f30fc2be726fb6b0edfeb289
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2014-02-26 15:13:25 -0300

    avoid repetition on features spec by using shared_examples

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -17,44 +17,38 @@ feature 'Sign in' do # , :js => true do
   given(:user) { FactoryGirl.create(:user) }
   given(:password) { FactoryGirl.build(:user).password }
 
-  scenario 'login with correct credentials' do
-    click_link 'Login'
-    within '#login-form' do
-      fill_in 'E-mail', :with => user.email
-      fill_in 'Password', :with => password
-      click_button "Log In"
+  shared_examples_for 'login' do |expected_response|
+    scenario 'log in and expect json response'do
+      click_link 'Login'
+      within '#login-form' do
+        fill_in 'E-mail', :with => user.email
+        fill_in 'Password', :with => password
+        click_button "Log In"
+      end
+      expect(page).to have_content(expected_response)
     end
-    expect(page).to have_content({:redirect => root_path}.to_json)
   end
 
-  scenario 'login with blank field' do
-    click_link 'Login'
-    within '#login-form' do
-      fill_in 'E-mail', :with => ''
-      fill_in 'Password', :with => ''
-      click_button "Log In"
-    end
-    expect(page).to have_content({:errors => "Fields can't be blank"}.to_json)
+  context 'correct credentials' do
+    given(:expected_response) { {:redirect => root_path}.to_json }
+    it_behaves_like 'login'
   end
 
-  scenario 'login with wrong credentials' do
-    click_link 'Login'
-    within '#login-form' do
-      fill_in 'E-mail', :with => user.email
-      fill_in 'Password', :with => 'wrong-pass'
-      click_button "Log In"
-    end
-    expect(page).to have_content({:errors => "E-mail or Password invalid"}.to_json)
+  context 'blank fields' do
+    given(:expected_response) { {:errors => "Fields can't be blank"}.to_json }
+    given(:password) { '' }
+    it_behaves_like 'login'
   end
 
-  scenario 'login with pending user' do
-    user = FactoryGirl.create(:pending_user)
-    click_link 'Login'
-    within '#login-form' do
-      fill_in 'E-mail', :with => user.email
-      fill_in 'Password', :with => password
-      click_button "Log In"
-    end
-    expect(page).to have_content({:errors => "Activation pending"}.to_json)
+  context 'wrong credentials' do
+    given(:expected_response) { {:errors => "E-mail or Password invalid"}.to_json }
+    given(:password) { 'wrong-pass' }
+    it_behaves_like 'login'
+  end
+
+  context 'pending user' do
+    given(:user) { FactoryGirl.create(:pending_user) }
+    given(:expected_response) { {:errors => "Activation pending"}.to_json }
+    it_behaves_like 'login'
   end
 end
