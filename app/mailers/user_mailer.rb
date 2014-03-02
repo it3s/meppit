b8d2c73f881ab558cb6f2f7a7e1c3ce8a61d3afc
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2014-03-02 20:36:59 -0300

    refactor user_mailer to avoid duplications

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,23 +1,23 @@
 class UserMailer < BaseMailer
 
-  def activation_email(user_id, lang=nil)
-    set_user_and_locale user_id, lang
-    @activation_url  = activate_user_url @user.activation_token
-    mail :to      => email_with_name(@user),
-         :subject => t('users.mail.welcome.subject')
+  def activation_email(user_id, locale=nil)
+    mail_with_token user_id, locale do |user| activate_user_url(user.activation_token) end
   end
 
-  def reset_password_email(user_id, lang=nil)
-    set_user_and_locale user_id, lang
-    @url  = edit_password_users_url(@user.reset_password_token)
-    mail :to      => email_with_name(@user),
-         :subject => t('user.forgot_password.mail_subject')
+  def reset_password_email(user_id, locale=nil)
+    mail_with_token user_id, locale do |user| edit_password_users_url(user.reset_password_token) end
   end
 
   private
 
-  def set_user_and_locale(user_id, lang)
-    I18n.locale = lang || I18n.default_locale
+  def mail_with_token(user_id, locale, &block)
+    set_user_and_locale user_id, locale
+    @url = block.call(@user)
+    mail :to => email_with_name(@user)
+  end
+
+  def set_user_and_locale(user_id, locale)
+    I18n.locale = locale || I18n.default_locale
     @user = User.find user_id
   end
 end
