329a27b8e18da51f67efed6d2acf8bdb84313f05
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2014-03-03 03:32:57 -0300

    improved toolbar partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -36,4 +36,20 @@ module ApplicationHelper
     options.deep_merge!(:remote => true, :html => {'data-components' => 'remoteForm'})
     simple_form_for(record, options, &block)
   end
+
+
+  def tools_list(obj, only=:all)
+    tools = {
+      :edit     => OpenStruct.new(:name => :edit,     :url => url_for([:edit, obj])),
+      :star     => OpenStruct.new(:name => :star,     :url => ""),
+      :comment  => OpenStruct.new(:name => :comment,  :url => ""),
+      :history  => OpenStruct.new(:name => :history,  :url => ""),
+      :flag     => OpenStruct.new(:name => :flag,     :url => ""),
+      :delete   => OpenStruct.new(:name => :delete,   :url => ""),
+      :google   => OpenStruct.new(:name => :google,   :url => ""),
+      :facebook => OpenStruct.new(:name => :facebook, :url => ""),
+      :twitter  => OpenStruct.new(:name => :twitter,  :url => ""),
+    }
+    only == :all ? tools.values() : only.map {|name| tools[name] }
+  end
 end
