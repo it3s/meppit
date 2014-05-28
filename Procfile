16beee65cd11c152c0a7619392190623315faa10
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2014-05-28 11:44:14 -0300

    add elasticsearch

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: bundle exec rails s
 worker: bundle exec sidekiq -C config/sidekiq.yml
+es: ./vendor/elasticsearch/bin/elasticsearch
--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ then
   apt-get upgrade -y
 
   # Install dependencies
-  apt-get install -y git ruby2.0 ruby2.0-dev redis-server postgresql-9.1 postgresql-contrib-9.1 postgresql-server-dev-9.1 libpq-dev nodejs libxslt1.1 imagemagick build-essential libtool checkinstall libjson0-dev libxml2-dev libproj-dev python2.7-dev swig libgeos-3.3.3 libgeos-dev gdal-bin libgdal1-1.9.0-grass libgdal1-dev
+  apt-get install -y git ruby2.0 ruby2.0-dev redis-server postgresql-9.1 postgresql-contrib-9.1 postgresql-server-dev-9.1 libpq-dev nodejs libxslt1.1 imagemagick build-essential libtool checkinstall libjson0-dev libxml2-dev libproj-dev python2.7-dev swig libgeos-3.3.3 libgeos-dev gdal-bin libgdal1-1.9.0-grass libgdal1-dev openjdk-7-jre
 
   # ========================
   # compile postgis
@@ -43,6 +43,13 @@ then
     sudo -u vagrant cp config/database.yml.sample config/database.yml
     sudo -u vagrant sed -i -e 's/username: postgres/username: vagrant/g' config/database.yml
   fi
+
+  # Install elasticsearch
+  wget -P vendor/ https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.2.0.tar.gz
+  tar xzvf vendor/elasticsearch-1.2.0.tar.gz -C vendor/
+  mv vendor/elasticsearch-1.2.0 vendor/elasticsearch
+  rm vendor/elasticsearch-1.2.0.tar.gz
+  chmod +x vendor/elasticsearch/bin/elasticsearch
 
   # Setup
   sudo -u vagrant bundle
