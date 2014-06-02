12111c7b2ac568c116f406bcd9afc193eb64d258
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2014-06-02 10:41:49 -0300

    test taggable

---
+++ spec/models/taggable_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe Taggable do
+  # TODO: user a dummy model to test taggable in isolation
+
+  let(:user) { FactoryGirl.build :user }
+
+  before { Tag.delete_all }
+
+  it { expect(User.method(:searchable_tags)).to_not raise_error }
+
+  it "sends callback and save tags" do
+    expect(Tag.all.map(&:tag)).to eq []
+    user.interests = ['aa', 'bb']
+    user.save
+    expect(Tag.all.map(&:tag)).to eq ['aa', 'bb']
+  end
+end
