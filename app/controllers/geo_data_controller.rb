862faf1b1e3885f8b8d1d71ff910215d01135f8b
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2014-06-09 14:21:32 -0300

    update geo_data

--- app/controllers/geo_data_controller.rb
+++ app/controllers/geo_data_controller.rb
@@ -9,7 +9,25 @@ class GeoDataController < ApplicationController
   def edit
   end
 
+  def update
+    @data.assign_attributes(data_params)
+    if @data.valid? && @data.save
+      flash[:notice] = t('geo_data.flash.updated')
+      render :json => {:redirect => geo_datum_path(@data)}
+    else
+      render :json => {:errors => @data.errors.messages}, :status => :unprocessable_entity
+    end
+  end
+
+
   private
+
+  def data_params
+    params.require(:geo_data).permit(:name, :description).tap do |whitelisted|
+      whitelisted[:contacts]  = (params[:geodata][:contacts]  || {}).delete_if { |key, value| value.blank? }
+      whitelisted[:tags] = (params[:geo_data][:tags] || '').split(',')
+    end
+  end
 
   def find_data
     @data = GeoData.find params[:id]
