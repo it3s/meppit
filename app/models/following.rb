8ced9e18db8ee6a5bf36fa0671c3cdf036258f32
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2014-06-11 12:40:01 -0300

    can follow an content only once

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,4 +1,6 @@
 class Following < ActiveRecord::Base
   belongs_to :follower, class_name: 'User'
   belongs_to :followable, polymorphic: true
+
+  validates :follower, uniqueness: {scope: [:followable_id, :followable_type]}
 end
