f832c3702d66d7ec9837509712b8163fe9ab7de3
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2014-06-13 14:44:15 -0300

    toolbar helper refactor

--- app/helpers/concerns/toolbar_helper.rb
+++ app/helpers/concerns/toolbar_helper.rb
@@ -3,31 +3,46 @@ module Concerns
     extend ActiveSupport::Concern
 
     def tools_list(obj, only=:all)
-      edit_url = url_for([:edit, obj])
-      tools = {
-        edit: {
-          icon:   :pencil,
-          title:   t('toolbar.edit'),
-          url:     edit_url,
-          active?: request.path == edit_url,
-        },
-        star:     {
-          icon:      :star,
-          title:     t('toolbar.star'),
-          url:       "#",
-          active?:   current_user && current_user.follow?(obj),
-          component: _follow_component(obj)
-        },
-        comment:  {icon: :comment,   title: t('toolbar.comment'),  url: ""},
-        history:  {icon: :'clock-o', title: t('toolbar.history'),  url: ""},
-        settings: {icon: :'cog',     title: t('toolbar.settings'), url: ""},
-        flag:     {icon: :flag,      title: t('toolbar.flag'),     url: ""},
-        delete:   {icon: :'trash-o', title: t('toolbar.delete'),   url: ""},
-      }
+      tools = {}
+      [:edit, :star, :comment, :history, :settings, :flag, :delete].each do |name|
+        tools[name] = send :"_#{name}_tool", obj
+      end
       only == :all ? tools.values() : only.map {|name| tools[name] }
     end
 
     private
+
+    def _edit_tool(obj)
+      edit_url = url_for([:edit, obj])
+      { icon: :pencil, title: t('toolbar.edit'), url: edit_url,
+        active?: request.path == edit_url }
+    end
+
+    def _star_tool(obj)
+      { icon: :star, title: t('toolbar.star'), url: "#",
+        active?: current_user && current_user.follow?(obj),
+        component: _follow_component(obj) }
+    end
+
+    def _comment_tool(obj)
+      { icon: :comment, title: t('toolbar.comment'), url: "" }
+    end
+
+    def _history_tool(obj)
+      { icon: :'clock-o', title: t('toolbar.history'), url: "" }
+    end
+
+    def _settings_tool(obj)
+      { icon: :'cog', title: t('toolbar.settings'), url: "" }
+    end
+
+    def _flag_tool(obj)
+      { icon: :flag, title: t('toolbar.flag'), url: "" }
+    end
+
+    def _delete_tool(obj)
+      { icon: :'trash-o', title: t('toolbar.delete'), url: "" }
+    end
 
     def _follow_component(obj)
       {
