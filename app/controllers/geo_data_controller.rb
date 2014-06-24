e590070d7e0876d055c075a0b84c57920a661321
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2014-06-23 22:08:32 -0300

    change hash syntax on geo_data_controller

--- app/controllers/geo_data_controller.rb
+++ app/controllers/geo_data_controller.rb
@@ -1,13 +1,13 @@
 class GeoDataController < ApplicationController
   include ContributableController
 
-  before_action :require_login, :only => [:edit, :update]
-  before_action :find_geo_data, :only => [:show, :edit, :update]
+  before_action :require_login, only: [:edit, :update]
+  before_action :find_geo_data, only: [:show, :edit, :update]
 
   def index
     @geo_data_collection = GeoData.page(params[:page]).per(params[:per])
     respond_to do |format|
-      format.html { render :layout => (request.xhr? ? false : "application") }
+      format.html { render layout: index_layout }
       format.js
     end
   end
@@ -39,5 +39,9 @@ class GeoDataController < ApplicationController
 
   def find_geo_data
     @geo_data = GeoData.find params[:id]
+  end
+
+  def index_layout
+    if request.xhr? then false else "application" end
   end
 end
