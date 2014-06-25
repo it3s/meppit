2b23f8590a80f4e2392e6a16e0c47df533d081ea
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2014-06-25 12:56:14 -0300

    close #47

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -2,18 +2,24 @@ class FollowingsController < ApplicationController
   before_action :require_login, :find_followable
 
   def create
-    @followable.add_follower current_user
-    render json: {ok: true}
+    follow_button_json_response @followable.add_follower(current_user)
   end
 
   def destroy
-    @followable.remove_follower current_user
-    render json: {ok: true}
+    follow_button_json_response @followable.remove_follower(current_user)
   end
 
   private
 
   def find_followable
     @followable = find_polymorphic_object
+  end
+
+  def follow_button_json_response(action_result)
+    if action_result
+      render json: {ok: true}
+    else
+      render json: {ok: false}, status: :unprocessable_entity
+    end
   end
 end
