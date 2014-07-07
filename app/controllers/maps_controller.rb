3fc9d79db96878d3158fd006a74c71ac5be5f6ee
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2014-07-07 13:33:13 -0300

    add contributable concern to maps controller

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,6 +1,8 @@
 class MapsController < ApplicationController
+  include ContributableController
+
   before_action :require_login, only: [:edit, :update]
-  before_action :find_map, only: [:show, :edit, :update]
+  before_action :find_map,      only: [:show, :edit, :update]
 
   def index
     @maps_collection = Map.page(params[:page]).per(params[:per])
@@ -18,6 +20,12 @@ class MapsController < ApplicationController
 
   def update
     update_object @map, map_params
+  end
+
+  protected
+
+  def after_update
+    save_contribution @map, current_user
   end
 
   private
