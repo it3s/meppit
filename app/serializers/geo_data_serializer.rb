0201349cc85f6f9bcbfb6a0eed33bdf55016b7ad
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2014-08-19 22:30:16 -0300

    improve relations serialization

--- app/serializers/geo_data_serializer.rb
+++ app/serializers/geo_data_serializer.rb
@@ -1,8 +1,8 @@
 class GeoDataSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :location, :additional_info, :contacts,
-             :tags, :relations
+             :tags, :relations, :created_at, :updated_at
 
   def relations
-    object.relations_values
+    object.relations_values(splitted_type: true)
   end
 end
