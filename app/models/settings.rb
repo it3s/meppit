572e5f3797923afd83419e7ec22121d6b063ee74
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2014-09-09 13:48:23 -0300

    add mail notification setting

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -5,13 +5,14 @@ class Settings
   include ActiveModel::Validations
 
 
-  attr_accessor :language, :auth_token, :old_password, :new_password
+  attr_accessor :language, :auth_token, :old_password, :new_password, :mail_notifications
 
   validates :new_password, confirmation: true
 
   def initialize(user)
     self.language = user.language
     self.auth_token = user.auth_token
+    self.mail_notifications = user.try(:mail_notifications) || 'daily'
   end
 
   def persisted?
