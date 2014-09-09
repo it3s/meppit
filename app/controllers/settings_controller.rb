e4f3b4d3df0b520a4002295439b9dd5ac8cd42d9
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2014-09-09 15:31:18 -0300

    settings form validation

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,18 +1,25 @@
 class SettingsController < ApplicationController
   before_action :require_login
-  before_action :find_user
+  before_action :set_user
 
   def show
   end
 
   def update
-    render json: {}
+    @settings.assign_attributes(params[:settings])
+    if @settings.valid? && @settings.save
+      flash[:notice] = t('flash.saved')
+      render json: {redirect: @user}
+    else
+      render json: {errors: @settings.errors.messages}, status: :unprocessable_entity
+    end
   end
 
   private
 
-    def find_user
-      @user = User.find(params[:id])
-      redirect_to root_path, notice: t('access_denied') unless @user == current_user
+    def set_user
+      redirect_to root_path, notice: t('access_denied') unless current_user.id == params[:id].to_i
+      @user = current_user
+      @settings = @user.settings
     end
 end
--- app/models/settings.rb
+++ app/models/settings.rb
@@ -5,14 +5,25 @@ class Settings
   include ActiveModel::Validations
 
 
-  attr_accessor :language, :auth_token, :old_password, :new_password, :mail_notifications
+  attr_accessor :user, :language, :auth_token, :old_password, :new_password,
+                :mail_notifications
 
+  validate  :change_password
   validates :new_password, confirmation: true
 
   def initialize(user)
+    self.user = user
     self.language = user.language
     self.auth_token = user.auth_token
     self.mail_notifications = user.try(:mail_notifications) || 'daily'
+  end
+
+  def assign_attributes(attrs)
+    attrs.each { |name, value| send "#{name}=", value }
+  end
+
+  def save
+    false
   end
 
   def persisted?
@@ -22,5 +33,15 @@ class Settings
   def id
     nil
   end
+
+  private
+
+    def change_password
+      unless [old_password, new_password, new_password_confirmation].all? &:blank?
+        errors.add :old_password, 'invalid password' unless User.authenticate(@user.email, old_password)
+        errors.add :new_password, 'provide a new password' if new_password.blank?
+        errors.add :new_password, 'password is too short' if new_password.size < 6
+      end
+    end
 
 end
