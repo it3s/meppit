40d9314427678e6a9cfe613188285f685661ed38
Luiz Armesto <luiz.armesto@gmail.com>
2014-10-17 12:34:41 -0300

    add 'partners' section to frontpage + improvements

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -6,31 +6,31 @@ module PagesHelper
       {
         :href  => '#use_cases#jornalismo_de_dados',
         :title => t('frontpage.what.journalism'),
-        :image => 'imgs/journalism.png',
+        :sprite => 'journalism',
         :text  => t('frontpage.what.journalism_explanation'),
       },
       {
         :href  => '#use_cases#mapeamento_de_redes',
         :title => t('frontpage.what.network'),
-        :image => 'imgs/network.png',
+        :sprite => 'network',
         :text  => t('frontpage.what.network_explanation'),
       },
       {
         :href  => '#use_cases#avaliacao_de_projetos_e_programas',
         :title => t('frontpage.what.evaluation'),
-        :image => 'imgs/evaluation.png',
+        :sprite => 'evaluation',
         :text  => t('frontpage.what.evaluation_explanation'),
       },
       {
         :href  => '#use_cases#diagnostico_de_territorio',
         :title => t('frontpage.what.diagnosis'),
-        :image => 'imgs/diagnosis.png',
+        :sprite => 'diagnosis',
         :text  => t('frontpage.what.diagnosis_explanation'),
       },
       {
         :href  => '#use_cases#guia_do_bairro',
         :title => t('frontpage.what.guide'),
-        :image => 'imgs/guide.png',
+        :sprite => 'guide',
         :text  => t('frontpage.what.guide_explanation'),
       },
     ]
