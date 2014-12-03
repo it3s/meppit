e1ece6e3112dfa4b5a1ad6ca5d441ae349cebe37
Luiz Armesto <luiz.armesto@gmail.com>
2014-12-02 23:40:37 -0200

    add `sort by distance` filter option

--- app/models/list_filter.rb
+++ app/models/list_filter.rb
@@ -3,7 +3,7 @@ class ListFilter
   extend ActiveModel::Naming
   include ActiveModel::Conversion
 
-  attr_accessor :tags, :tags_type, :sort_by, :order, :visualization
+  attr_accessor :tags, :tags_type, :sort_by, :order, :visualization, :longitude, :latitude
 
   def initialize(attributes = {})
     attributes.each { |name, value| send "#{name}=", value }
@@ -12,7 +12,7 @@ class ListFilter
 
   def filter(queryset)
     queryset = queryset.with_tags(tags, tags_type.to_sym) unless tags.empty?
-    queryset = queryset.order ordering
+    queryset = sort queryset
     queryset
   end
 
@@ -42,7 +42,18 @@ class ListFilter
       self.visualization ||= 'list'
     end
 
-    def ordering
-      "#{sort_by} #{order}"
+    def sort(queryset)
+      _sort_by = sort_by
+      if _sort_by == 'location'
+        if queryset.respond_to?(:nearest)
+          queryset = queryset.nearest(longitude, latitude)
+        else
+          _sort_by = 'name'
+        end
+      else
+        queryset = queryset.order "#{_sort_by} #{order}"
+      end
+      queryset
     end
+
 end
