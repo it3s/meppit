89a8ccc42895693d4d3140f42ad337dbc5314c0a
Luiz Armesto <luiz.armesto@gmail.com>
2014-12-18 13:20:25 -0200

    specs

--- spec/requests/api/v1/geo_data_spec.rb
+++ spec/requests/api/v1/geo_data_spec.rb
@@ -34,6 +34,13 @@ describe "API::V1::GeoData" do
         it { expect(response.status).to eq 200 }
         it { expect(response.content_type).to eq Mime::XML }
       end
+
+      context "geojson" do
+        let(:headers) { {'Authorization'=>"Token token=#{user.auth_token}", "Accept"=>Mime::GEOJSON} }
+
+        it { expect(response.status).to eq 200 }
+        it { expect(response.content_type).to eq Mime::GEOJSON }
+      end
     end
   end
 
@@ -66,6 +73,47 @@ describe "API::V1::GeoData" do
 
         it { expect(response.status).to eq 200 }
         it { expect(response.content_type).to eq Mime::XML }
+      end
+
+      context "geojson" do
+        let(:headers) { {'Authorization'=>"Token token=#{user.auth_token}", "Accept"=>Mime::GEOJSON} }
+
+        it { expect(response.status).to eq 200 }
+        it { expect(response.content_type).to eq Mime::GEOJSON }
+      end
+    end
+
+
+    context "params" do
+      let (:headers) { {'Authorization'=>"Token token=#{user.auth_token}", "Accept"=>Mime::JSON} }
+      let(:geoms) do
+       [[10, 10], [20, 10],  [30, 10], [40, 10]
+       ].collect { |lon, lat| RGeo::Cartesian.simple_factory.parse_wkt "POINT (#{lon} #{lat})"  }
+      end
+      before do
+        [['b', geoms[1]], ['c', geoms[2]], ['a', geoms[0]], ['d', geoms[3]]
+        ].each { |n, l| FactoryGirl.create :geo_data, name: n, location: l, tags: ['z', n] }
+      end
+      describe "pagination" do
+        it "gets the correct number of objects per page" do
+          get "/api/v1/geo_data", {page: 1, per: 2}, headers
+          expect(JSON::parse(response.body).count).to eq 2
+        end
+        it "gets the correct objects" do
+          get "/api/v1/geo_data", {page: 2, per: 2}, headers
+          expect(JSON::parse(response.body).map{ |item| item["name"] }).to match_array ["a", "d"]
+        end
+        it "gets the pagination headers" do
+          get "/api/v1/geo_data", {page: 2, per: 2}, headers
+          expect(response.headers.has_key? "Link").to be true
+          expect(response.headers.has_key? "X-Per-Page").to be true
+          expect(response.headers.has_key? "X-Pages").to be true
+          expect(response.headers.has_key? "X-Total-Count").to be true
+
+          expect(response.headers["X-Per-Page"]).to eq "2"
+          expect(response.headers["X-Pages"]).to eq "2"
+          expect(response.headers["X-Total-Count"]).to eq "4"
+        end
       end
     end
   end
--- spec/requests/api/v1/maps_spec.rb
+++ spec/requests/api/v1/maps_spec.rb
@@ -34,6 +34,13 @@ describe "API::V1::Maps" do
         it { expect(response.status).to eq 200 }
         it { expect(response.content_type).to eq Mime::XML }
       end
+
+      context "geojson" do
+        let(:headers) { {'Authorization'=>"Token token=#{user.auth_token}", "Accept"=>Mime::GEOJSON} }
+
+        it { expect(response.status).to eq 200 }
+        it { expect(response.content_type).to eq Mime::GEOJSON }
+      end
     end
   end
 
@@ -66,6 +73,41 @@ describe "API::V1::Maps" do
 
         it { expect(response.status).to eq 200 }
         it { expect(response.content_type).to eq Mime::XML }
+      end
+
+      context "geojson" do
+        let(:headers) { {'Authorization'=>"Token token=#{user.auth_token}", "Accept"=>Mime::GEOJSON} }
+
+        it { expect(response.status).to eq 200 }
+        it { expect(response.content_type).to eq Mime::GEOJSON }
+      end
+    end
+
+    context "params" do
+      let (:headers) { {'Authorization'=>"Token token=#{user.auth_token}", "Accept"=>Mime::JSON} }
+      before do
+        ['b', 'c', 'a', 'd'].each { |n| FactoryGirl.create :map, name: n, tags: ['z', n] }
+      end
+      describe "pagination" do
+        it "gets the correct number of objects per page" do
+          get "/api/v1/maps", {page: 1, per: 2}, headers
+          expect(JSON::parse(response.body).count).to eq 2
+        end
+        it "gets the correct objects" do
+          get "/api/v1/maps", {page: 2, per: 2}, headers
+          expect(JSON::parse(response.body).map{ |item| item["name"] }).to match_array ["a", "d"]
+        end
+        it "gets the pagination headers" do
+          get "/api/v1/maps", {page: 2, per: 2}, headers
+          expect(response.headers.has_key? "Link").to be true
+          expect(response.headers.has_key? "X-Per-Page").to be true
+          expect(response.headers.has_key? "X-Pages").to be true
+          expect(response.headers.has_key? "X-Total-Count").to be true
+
+          expect(response.headers["X-Per-Page"]).to eq "2"
+          expect(response.headers["X-Pages"]).to eq "2"
+          expect(response.headers["X-Total-Count"]).to eq "4"
+        end
       end
     end
   end
