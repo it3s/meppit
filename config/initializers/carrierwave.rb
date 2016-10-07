9e20110fa4b806c002b96704ceb53c74c7770568
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2016-10-07 12:47:58 -0300

    fix carrierwave settings

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,13 +5,15 @@ if Rails.env.test? || Rails.env.development?
   end
 else
   CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       :provider               => 'AWS',
       :aws_access_key_id      => ENV["S3_KEY"],
       :aws_secret_access_key  => ENV["S3_SECRET"],
     }
-    config.asset_host     = "https://s3.amazonaws.com/#{ENV["S3_BUCKET"]}"
+    # config.asset_host     = "https://s3.amazonaws.com/#{ENV["S3_BUCKET"]}"
+    config.cache_dir = "#{Rails.root}/tmp/uploads"
     config.fog_directory  = ENV["S3_BUCKET"]
+    # config.fog_public = false
+    condig.storage = :fog
   end
 end
