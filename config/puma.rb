45d7bd7ac81f191bf7b8e7d65d173956925bc7d0
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2016-10-07 13:44:20 -0300

    Update puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,20 +1,26 @@
-workers Integer(ENV['PUMA_WORKERS'] || 3)
-threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 16)
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
 
 preload_app!
 
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
-bind 'unix://tmp/sockets/puma.sock'
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
-  # worker specific setup
-  ActiveSupport.on_load(:active_record) do
-    config = ActiveRecord::Base.configurations[Rails.env] ||
-                Rails.application.config.database_configuration[Rails.env]
-    config['pool'] = ENV['MAX_THREADS'] || 16
-    config['adapter'] = 'postgis'
-    ActiveRecord::Base.establish_connection(config)
-  end
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
 end
+
+# on_worker_boot do
+#   # worker specific setup
+#   ActiveSupport.on_load(:active_record) do
+#     config = ActiveRecord::Base.configurations[Rails.env] ||
+#                 Rails.application.config.database_configuration[Rails.env]
+#     config['pool'] = ENV['MAX_THREADS'] || 16
+#     config['adapter'] = 'postgis'
+#     ActiveRecord::Base.establish_connection(config)
+#   end
+# end
